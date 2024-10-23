4d5da84f1c14618c632da400d7c1c1869430d908
eyasar23 <eyasar23@ku.edu.tr>
2024-10-23 23:35:45 +0300

    b
    n

--- ftk.py
+++ ftk.py
@@ -2,8 +2,10 @@ def cone_vol(r,h,pi):
     area = pi*r**2*h*(1/3)
     return area
 print(cone_vol(17,12,3.14))
-def f(x1,x2,y1,y2):
-    m = (y2-y1)/(x2-x1)
-    k = -(m*x1)+y1
-    return k
-print(f(26,10,33,18))
+def interest_calculator(P,r,t):
+    A = P * (1 + (r / 100))**t
+    return A
+def average(x1,x2,y,z):
+    y= ((x1+x2)/2 )**z 
+    return y
+print(average(2,3,5,235))
